5c2884a84e6c9f30abd9fb4160b347da07912a12
Simon Southwell <simon@anita-simulators.org.uk>
2024-11-05 13:30:43 +0000

    Updated order of prototypes

--- src/mem.h
+++ src/mem.h
@@ -71,10 +71,6 @@ extern void   InitialiseMem       (int node);
 
 extern void     WriteRamByteBlock   (const uint64_t addr, const PktData_t* const data, const int fbe, const int lbe, const int length, const uint32_t node);
 extern int      ReadRamByteBlock    (const uint64_t addr, PktData_t* const data, const int length, const uint32_t node);
-extern void     WriteConfigSpace    (const uint32_t addr, const uint32_t data, const uint32_t node);
-extern uint32_t ReadConfigSpace     (const uint32_t addr, const uint32_t node);
-extern void     WriteConfigSpaceBuf (const uint32_t addr, const PktData_t* const data, const int fbe, const int lbe, const int length, const uint32_t node);
-extern void     ReadConfigSpaceBuf  (const uint32_t addr, PktData_t* const data, const int length, const uint32_t node);
 
 extern void     WriteRamByte        (const uint64_t addr, const uint32_t data, const uint32_t node);
 extern void     WriteRamWord        (const uint64_t addr, const uint32_t data, const int little_endian, const uint32_t node);
@@ -82,4 +78,10 @@ extern void     WriteRamDWord       (const uint64_t addr, const uint64_t data, c
 extern uint32_t ReadRamByte         (const uint64_t addr, const uint32_t node);
 extern uint32_t ReadRamWord         (const uint64_t addr, const int little_endian, const uint32_t node);
 extern uint64_t ReadRamDWord        (const uint64_t addr, const int little_endian, const uint32_t node);
+
+extern void     WriteConfigSpace    (const uint32_t addr, const uint32_t data, const uint32_t node);
+extern uint32_t ReadConfigSpace     (const uint32_t addr, const uint32_t node);
+extern void     WriteConfigSpaceBuf (const uint32_t addr, const PktData_t* const data, const int fbe, const int lbe, const int length, const uint32_t node);
+extern void     ReadConfigSpaceBuf  (const uint32_t addr, PktData_t* const data, const int length, const uint32_t node);
+
 #endif
