a538e2e3dcf54ecf1055eba6094673877998013b
Simon Southwell <simon@anita-simulators.org.uk>
2025-11-20 09:55:37 +0000

    Fixed an issue on scrambler when 8b10b disabled

--- src/codec.c
+++ src/codec.c
@@ -1,5 +1,5 @@
 //=============================================================
-// 
+//
 // Copyright (c) 2016 Simon Southwell. All rights reserved.
 //
 // Date: 20th Sep 2016
@@ -69,10 +69,10 @@ static const TblType PosTable5[32] = {
     {0x34,  0}, {0x15,  0}, {0x16,  0}, {0x28, -2},
     {0x0c, -2}, {0x19,  0}, {0x1a,  0}, {0x24, -2},
     {0x1c,  0}, {0x22, -2}, {0x21, -2}, {0x0a, -2},
-}; 
+};
 
 // NegTable3 and PosTable3 have equal values when disparity is 0,
-// otherwise entries are inverse. 
+// otherwise entries are inverse.
 
 static const TblType NegTable3[8] = {
     {0x2, -2}, {0x9,  0}, {0xa,  0}, {0xc,  0},
@@ -109,51 +109,51 @@ static const TblType K_PosTable3[12] = {
     {0xe,  2}, {0xe,  2}, {0xe,  2}, {0xe,  2},
 };
 
-static const unsigned int Bitrev4 [16] = 
-                                   {0x0, 0x8, 0x4, 0xc, 
-                                    0x2, 0xa, 0x6, 0xe, 
-                                    0x1, 0x9, 0x5, 0xd, 
+static const unsigned int Bitrev4 [16] =
+                                   {0x0, 0x8, 0x4, 0xc,
+                                    0x2, 0xa, 0x6, 0xe,
+                                    0x1, 0x9, 0x5, 0xd,
                                     0x3, 0xb, 0x7, 0xf};
 
 
-static const unsigned int NumOfOnes4 [16] = 
+static const unsigned int NumOfOnes4 [16] =
                                       {0, 1, 1, 2,
                                        1, 2, 2, 3,
                                        1, 2, 2, 3,
                                        2, 3, 3, 4};
 
 unsigned const int Bitrev8 [256] = {
-    0x00, 0x80, 0x40, 0xc0, 0x20, 0xa0, 0x60, 0xe0, 
-    0x10, 0x90, 0x50, 0xd0, 0x30, 0xb0, 0x70, 0xf0, 
-    0x08, 0x88, 0x48, 0xc8, 0x28, 0xa8, 0x68, 0xe8, 
-    0x18, 0x98, 0x58, 0xd8, 0x38, 0xb8, 0x78, 0xf8, 
-    0x04, 0x84, 0x44, 0xc4, 0x24, 0xa4, 0x64, 0xe4, 
-    0x14, 0x94, 0x54, 0xd4, 0x34, 0xb4, 0x74, 0xf4, 
-    0x0c, 0x8c, 0x4c, 0xcc, 0x2c, 0xac, 0x6c, 0xec, 
-    0x1c, 0x9c, 0x5c, 0xdc, 0x3c, 0xbc, 0x7c, 0xfc, 
-    0x02, 0x82, 0x42, 0xc2, 0x22, 0xa2, 0x62, 0xe2, 
-    0x12, 0x92, 0x52, 0xd2, 0x32, 0xb2, 0x72, 0xf2, 
-    0x0a, 0x8a, 0x4a, 0xca, 0x2a, 0xaa, 0x6a, 0xea, 
-    0x1a, 0x9a, 0x5a, 0xda, 0x3a, 0xba, 0x7a, 0xfa, 
-    0x06, 0x86, 0x46, 0xc6, 0x26, 0xa6, 0x66, 0xe6, 
-    0x16, 0x96, 0x56, 0xd6, 0x36, 0xb6, 0x76, 0xf6, 
-    0x0e, 0x8e, 0x4e, 0xce, 0x2e, 0xae, 0x6e, 0xee, 
-    0x1e, 0x9e, 0x5e, 0xde, 0x3e, 0xbe, 0x7e, 0xfe, 
-    0x01, 0x81, 0x41, 0xc1, 0x21, 0xa1, 0x61, 0xe1, 
-    0x11, 0x91, 0x51, 0xd1, 0x31, 0xb1, 0x71, 0xf1, 
-    0x09, 0x89, 0x49, 0xc9, 0x29, 0xa9, 0x69, 0xe9, 
-    0x19, 0x99, 0x59, 0xd9, 0x39, 0xb9, 0x79, 0xf9, 
-    0x05, 0x85, 0x45, 0xc5, 0x25, 0xa5, 0x65, 0xe5, 
-    0x15, 0x95, 0x55, 0xd5, 0x35, 0xb5, 0x75, 0xf5, 
-    0x0d, 0x8d, 0x4d, 0xcd, 0x2d, 0xad, 0x6d, 0xed, 
-    0x1d, 0x9d, 0x5d, 0xdd, 0x3d, 0xbd, 0x7d, 0xfd, 
-    0x03, 0x83, 0x43, 0xc3, 0x23, 0xa3, 0x63, 0xe3, 
-    0x13, 0x93, 0x53, 0xd3, 0x33, 0xb3, 0x73, 0xf3, 
-    0x0b, 0x8b, 0x4b, 0xcb, 0x2b, 0xab, 0x6b, 0xeb, 
-    0x1b, 0x9b, 0x5b, 0xdb, 0x3b, 0xbb, 0x7b, 0xfb, 
-    0x07, 0x87, 0x47, 0xc7, 0x27, 0xa7, 0x67, 0xe7, 
-    0x17, 0x97, 0x57, 0xd7, 0x37, 0xb7, 0x77, 0xf7, 
-    0x0f, 0x8f, 0x4f, 0xcf, 0x2f, 0xaf, 0x6f, 0xef, 
+    0x00, 0x80, 0x40, 0xc0, 0x20, 0xa0, 0x60, 0xe0,
+    0x10, 0x90, 0x50, 0xd0, 0x30, 0xb0, 0x70, 0xf0,
+    0x08, 0x88, 0x48, 0xc8, 0x28, 0xa8, 0x68, 0xe8,
+    0x18, 0x98, 0x58, 0xd8, 0x38, 0xb8, 0x78, 0xf8,
+    0x04, 0x84, 0x44, 0xc4, 0x24, 0xa4, 0x64, 0xe4,
+    0x14, 0x94, 0x54, 0xd4, 0x34, 0xb4, 0x74, 0xf4,
+    0x0c, 0x8c, 0x4c, 0xcc, 0x2c, 0xac, 0x6c, 0xec,
+    0x1c, 0x9c, 0x5c, 0xdc, 0x3c, 0xbc, 0x7c, 0xfc,
+    0x02, 0x82, 0x42, 0xc2, 0x22, 0xa2, 0x62, 0xe2,
+    0x12, 0x92, 0x52, 0xd2, 0x32, 0xb2, 0x72, 0xf2,
+    0x0a, 0x8a, 0x4a, 0xca, 0x2a, 0xaa, 0x6a, 0xea,
+    0x1a, 0x9a, 0x5a, 0xda, 0x3a, 0xba, 0x7a, 0xfa,
+    0x06, 0x86, 0x46, 0xc6, 0x26, 0xa6, 0x66, 0xe6,
+    0x16, 0x96, 0x56, 0xd6, 0x36, 0xb6, 0x76, 0xf6,
+    0x0e, 0x8e, 0x4e, 0xce, 0x2e, 0xae, 0x6e, 0xee,
+    0x1e, 0x9e, 0x5e, 0xde, 0x3e, 0xbe, 0x7e, 0xfe,
+    0x01, 0x81, 0x41, 0xc1, 0x21, 0xa1, 0x61, 0xe1,
+    0x11, 0x91, 0x51, 0xd1, 0x31, 0xb1, 0x71, 0xf1,
+    0x09, 0x89, 0x49, 0xc9, 0x29, 0xa9, 0x69, 0xe9,
+    0x19, 0x99, 0x59, 0xd9, 0x39, 0xb9, 0x79, 0xf9,
+    0x05, 0x85, 0x45, 0xc5, 0x25, 0xa5, 0x65, 0xe5,
+    0x15, 0x95, 0x55, 0xd5, 0x35, 0xb5, 0x75, 0xf5,
+    0x0d, 0x8d, 0x4d, 0xcd, 0x2d, 0xad, 0x6d, 0xed,
+    0x1d, 0x9d, 0x5d, 0xdd, 0x3d, 0xbd, 0x7d, 0xfd,
+    0x03, 0x83, 0x43, 0xc3, 0x23, 0xa3, 0x63, 0xe3,
+    0x13, 0x93, 0x53, 0xd3, 0x33, 0xb3, 0x73, 0xf3,
+    0x0b, 0x8b, 0x4b, 0xcb, 0x2b, 0xab, 0x6b, 0xeb,
+    0x1b, 0x9b, 0x5b, 0xdb, 0x3b, 0xbb, 0x7b, 0xfb,
+    0x07, 0x87, 0x47, 0xc7, 0x27, 0xa7, 0x67, 0xe7,
+    0x17, 0x97, 0x57, 0xd7, 0x37, 0xb7, 0x77, 0xf7,
+    0x0f, 0x8f, 0x4f, 0xcf, 0x2f, 0xaf, 0x6f, 0xef,
     0x1f, 0x9f, 0x5f, 0xdf, 0x3f, 0xbf, 0x7f, 0xff};
 
 // -------------------------------------------------------------------------
@@ -190,7 +190,7 @@ static void ScrambleAdvance(const int reset, uint32_t* const lfsr)
 
 unsigned int Encode (const int data, const int no_scramble, const int no_8b10b, const int lane, const int linkwidth, const int node)
 {
-    unsigned int code, code3, code5, c, tblidx; 
+    unsigned int code, code3, code5, c, tblidx;
     const TblType *tbl5, *tbl3;
 
     if (!no_scramble && data <= 0xff)
@@ -202,37 +202,42 @@ unsigned int Encode (const int data, const int no_scramble, const int no_8b10b, 
         c = data;
     }
 
+    // When scrambling disabled, hold LSRF in reset state
+    if (no_scramble)
+    {
+        ScrambleAdvance(true, &(this->elfsr));
+    }
+    // Advance scrambler unless a SKIP, or reset if COMMA
+    else if (data != SKP && lane == (linkwidth-1))
+    {
+        ScrambleAdvance(data == COM, &(this->elfsr));
+    }
+
     if (!no_8b10b)
     {
-        // Advance scrambler unless a SKIP, or reset if COMMA
-        if (data != SKP && lane == (linkwidth-1))
-        {
-            ScrambleAdvance(data == COM, &(this->elfsr));
-        }
-        
         // For data bytes
         if (c < 256)
         {
             // Pick 5 bit table based on running disparity
             tbl5 = (this->rd[lane] == -1) ? NegTable5 : PosTable5;
-        
-            // 3 bit table based in the disparity of the 5 bit table and current 
+
+            // 3 bit table based in the disparity of the 5 bit table and current
             // running disparity. If total Positive, choose NegTable3 else PosTable3.
             tbl3 = (tbl5[c & 0x1f].disparity == (this->rd[lane] == 1 ? 0 : 2)) ? NegTable3 : PosTable3;
-        
+
             // Extract codes
             code5 = tbl5[c & 0x1f].code;
-            code3 = tbl3[c >> 5].code ; 
-        
+            code3 = tbl3[c >> 5].code ;
+
             // Bit reverse 3b/4b code to avoid a run of 5 bits
-            if (((code5 & 0x30) == 0x30 && (code3 & 0x7) == 0x7) || ((code5 & 0x30) == 0x0 && (code3 & 0x7) == 0x0)) 
+            if (((code5 & 0x30) == 0x30 && (code3 & 0x7) == 0x7) || ((code5 & 0x30) == 0x0 && (code3 & 0x7) == 0x0))
             {
                 code3 = Bitrev4[code3];
             }
-        
+
             // Calculate new running disparity
             this->rd[lane] += tbl5[c & 0x1f].disparity  +  tbl3[c >> 5].disparity;
-        
+
         // For control codes
         }
         else
@@ -242,25 +247,25 @@ unsigned int Encode (const int data, const int no_scramble, const int no_8b10b, 
             {
                 tblidx = (c >> 5) & 0x7;
             }
-            else 
+            else
             {
                 tblidx = (c == 0x1f7) ? 8 :
                          (c == 0x1fb) ? 9 :
                          (c == 0x1fd) ? 10 :
                                         11;
             }
-        
+
             tbl5 = (this->rd[lane] == -1) ? K_NegTable5 : K_PosTable5;
-        
+
             tbl3 = (tbl5[tblidx].disparity == (this->rd[lane] == 1 ? 0 : 2)) ? K_NegTable3 : K_PosTable3;
-        
+
             code5 = tbl5[tblidx].code;
             code3 = tbl3[tblidx].code;
-        
+
             // Calculate new running disparity
             this->rd[lane] += tbl5[tblidx].disparity + tbl3[tblidx].disparity;
         }
-        
+
         // Construct 10 bit code
         code = code3 << 6 | code5;
     }
@@ -268,7 +273,7 @@ unsigned int Encode (const int data, const int no_scramble, const int no_8b10b, 
     {
         code = c;
     }
-    
+
     return code;
 }
 
@@ -290,12 +295,12 @@ unsigned int Decode (const int data, const int no_scramble, const int no_8b10b, 
         int Invert31, Invert420, Invert42, Invert310, InvertAll5;
         int InvertMask5, InvertAll3, InvertMask3;
         int LowBits, Hibits;
-        
+
         // Run length flags of input first 4 bits
         TwoOnes    = NumOfOnes4[data&0xf] == 2;
         ThreeZeros = NumOfOnes4[data&0xf] == 1;
         ThreeOnes  = NumOfOnes4[data&0xf] == 3;
-        
+
         // Calculate bit inversion requirements for 6b/5b code
         Invert21   = (TwoOnes     && Data2and1 && Data5xnor4);
         Invert430  = (TwoOnes     && Data2nor1 && Data5xnor4);
@@ -306,33 +311,33 @@ unsigned int Decode (const int data, const int no_scramble, const int no_8b10b, 
         Invert42   = (Data1nor0   && Data5nor4);
         Invert310  = (Data1and0   && Data5and4);
         InvertAll5 = ((ThreeZeros && (Data5to3and || !Data4)) || Data5to2nor);
-        
+
         // Create an inversion mask for 6b/5b code
-        InvertMask5 = ((Invert430 | Invert4    | Invert420 | Invert42  | InvertAll5) << 4) | 
+        InvertMask5 = ((Invert430 | Invert4    | Invert420 | Invert42  | InvertAll5) << 4) |
                       ((Invert430 | Invert3210 | Invert31  | Invert310 | InvertAll5) << 3) |
                       ((Invert21  | Invert3210 | Invert420 | Invert42  | InvertAll5) << 2) |
                       ((Invert21  | Invert3210 | Invert31  | Invert310 | InvertAll5) << 1) |
-                      ((Invert430 | Invert3210 | Invert420 | Invert310 | InvertAll5) << 0); 
-        
+                      ((Invert430 | Invert3210 | Invert420 | Invert310 | InvertAll5) << 0);
+
         // Low order 5 bits
         LowBits =  (data & 0x1f) ^ InvertMask5;
-        
+
         // Flag condition where all 3 bits need inverting
         InvertAll3 = (Data5to2nor && Data9xor8) || (Data7nor6 && Data9and8) || (Data7and6 && Data9) || (Data8to6nor);
-        
+
         // Create an inversion mask for 4b/3b code
         InvertMask3 = (
                        (((Data9and8 &&  Data6) || Data9to7nor) << 2) |
                        (((Data9nor8 && !Data6) || Data9to7nor) << 1) |
                        (((Data9and8 &&  Data6) || Data9to7and)     )) | (InvertAll3 ? 0x07 : 0);
-        
+
         // High order 3 bits
         Hibits = ((data >> 6) & 0x7) ^ InvertMask3;
-        
-        
+
+
         // Output is a control code, not a data byte
         Control = (Data5to2and) || (Data5to2nor) || (ThreeZeros && !Data4 && Data5 && Data9to7and) || (ThreeOnes && Data4 && !Data5 & Data9to7nor);
-        
+
         Raw = (Control << 8) | (Hibits << 5) | LowBits;
     }
     else
@@ -343,7 +348,7 @@ unsigned int Decode (const int data, const int no_scramble, const int no_8b10b, 
 
     // Scrambling
 
-    // Decide if we're receiving a training sequence 
+    // Decide if we're receiving a training sequence
     if (lane == 0)
     {
         if (this->last_lane0_sym == COM && (Raw == PAD || !Control))
@@ -358,12 +363,12 @@ unsigned int Decode (const int data, const int no_scramble, const int no_8b10b, 
     this->last_lane0_sym = Raw;
 
     if (!this->ts_active && !no_scramble && Raw <= 0xff)
-    {        
+    {
         Raw = Raw ^ Bitrev8[(this->dlfsr & 0xff00) >> 8];
     }
 
-    // Advance scrambler unless a SKIP, or reset if COMMA
-    if (Raw != SKP && lane == (linkwidth-1))
+    // Advance scrambler unless a SKIP or in a training sequence, or reset if COMMA
+    if (!this->ts_active && !no_scramble && Raw != SKP && lane == (linkwidth-1))
     {
         ScrambleAdvance(Raw == COM, &(this->dlfsr));
     }
@@ -414,7 +419,7 @@ void InitCodec (const int node)
     {
         this->rd[idx]    = 1;
     }
-    
+
     this->elfsr = DEFAULTLFSRVALUE;
     this->dlfsr = DEFAULTLFSRVALUE;
 
--- src/pcie.h
+++ src/pcie.h
@@ -65,7 +65,7 @@
 
 #define PCIE_MAJOR_VER                    1
 #define PCIE_MINOR_VER                    6
-#define PCIE_PATCH_VER                    10
+#define PCIE_PATCH_VER                    11
 
 // Used in macros
 #define BYTE_MASK                         0xff
