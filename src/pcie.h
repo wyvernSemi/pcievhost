68a1a591cf984e6c27b97dc386bd9afb1907af58
Simon Southwell <simon@anita-simulators.org.uk>
2026-06-10 11:02:39 +0100

    Set version to 1.9.1

--- src/pcie.h
+++ src/pcie.h
@@ -65,7 +65,7 @@
 
 #define PCIE_MAJOR_VER                    1
 #define PCIE_MINOR_VER                    9
-#define PCIE_PATCH_VER                    0
+#define PCIE_PATCH_VER                    1
 
 // Used in macros
 #define BYTE_MASK                         0xff
